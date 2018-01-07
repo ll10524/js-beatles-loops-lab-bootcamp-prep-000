c25e5066ce169957652c3ff6b8fe6e6a866ae09d
Leo Tu <ll410119@gmail.com>
2018-01-07 03:07:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function theBeatlesPlay(musicians, instruments) {
   var result = [];
   
   for (let i=0; i<musicians.length; i++) {
-    word = ``;
+    word = `${musicians[i]} plays ${instruments[i]}`;
   }
 }
